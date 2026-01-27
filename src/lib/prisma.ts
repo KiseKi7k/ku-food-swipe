deaacccc92588cbcc4cc924453f0b9d459baee59
KiseKi7k <topzanahee@gmail.com>
2026-01-27 22:53:37 +0700

    feat: install better-auth

---
+++ src/lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/client";
+import { Pool } from "pg";
+
+const globalForPrisma = global as unknown as {
+  prisma?: PrismaClient;
+  pgPool?: Pool;
+};
+
+const pool =
+  globalForPrisma.pgPool ??
+  new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg(pool),
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.pgPool = pool;
+  globalForPrisma.prisma = prisma;
+}
