59b9124e0ab365b5b9240e3c881fc2d54dab69da
pardparw <pheemmpong7163@gmail.com>
2026-03-22 12:47:38 +0700

    delete unnecessary file

--- src/app/utils/checkCookie.tsx
+++
@@ -1,5 +0,0 @@
-
-export const checkCookie = (cookieName:string): string | null => {
-    const cookies = document.cookie.split("; ").find((row) => row.startsWith(`${cookieName}=`));
-    return cookies ? cookies.split("=")[1] : null;
-};
