bb80538651fbe15fd5e6e2043b99eb51c97d1a39
KiseKi7k <pornchaiwannasuk@gmail.com>
2026-04-05 06:12:24 +0700

    feat: update schema and post service
    Added post service for create, get and vote post
    Update schema
    - Added post
    - Added image in item
    Update item query in item service

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const bytesToBase64 = (buffer: Buffer | Uint8Array): string => {
 	return Buffer.from(buffer).toString("base64");
 };
+
+export const getImageBase64 = (image: Buffer | Uint8Array): string => {
+	return `data:image/jpeg;base64,${bytesToBase64(image)}`;
+};
---
+++ src/service/post.service.ts
@@ -0,0 +1,266 @@
+import prisma from "@/lib/prisma";
+import { getImageBase64 } from "@/lib/utils";
+
+type createPostBody = {
+	priceMin: number;
+	priceMax?: number;
+	shopId: string;
+	image: Buffer<ArrayBuffer>;
+} & (
+	| {
+			createFood: false;
+			foodId: string;
+	  }
+	| {
+			createFood: true;
+			foodName: string;
+			tags: string[];
+	  }
+);
+
+type Post = {
+	id: string;
+	upvotes: number;
+	downvotes: number;
+	food: {
+		name: string;
+		tags: string[];
+		shop: string;
+		location: string;
+		image: string | null;
+	};
+	creator: {
+		id: string;
+		name: string;
+		image: string | null;
+	};
+	createdAt: Date;
+};
+
+type ErrorType = {
+	success: false;
+	message: string;
+};
+
+export const postService = {
+	createPost: async (
+		userId: string,
+		data: createPostBody,
+	): Promise<Post | ErrorType> => {
+		try {
+			const user = await prisma.user.findUnique({
+				where: { id: userId },
+			});
+
+			if (!user) throw new Error("User not found");
+
+			const result = await prisma.$transaction(async (tx) => {
+				let food;
+
+				if (data.createFood) {
+					food = await tx.food.create({
+						data: {
+							name: data.foodName,
+							image: data.image,
+							tags: {
+								connectOrCreate: data.tags.map((tagName) => ({
+									where: { name: tagName },
+									create: { name: tagName },
+								})),
+							},
+						},
+						include: {
+							tags: { select: { name: true } },
+						},
+					});
+				} else {
+					food = await tx.food.findUnique({
+						where: { id: data.foodId },
+						include: {
+							tags: { select: { name: true } },
+						},
+					});
+
+					if (!food) throw new Error("Food not found");
+				}
+
+				const item = await tx.item.create({
+					data: {
+						priceMin: data.priceMin,
+						priceMax: data.priceMax,
+						shopId: data.shopId,
+						foodId: food.id,
+					},
+					include: {
+						shop: {
+							select: {
+								name: true,
+								location: { select: { name: true } },
+							},
+						},
+					},
+				});
+
+				const post = await tx.post.create({
+					data: {
+						creatorId: userId,
+						itemId: item.id,
+					},
+					include: {
+						creator: {
+							select: {
+								id: true,
+								name: true,
+								image: true,
+							},
+						},
+					},
+				});
+
+				return {
+					...post,
+					food: {
+						name: food.name,
+						tags: food.tags.map((t) => t.name),
+						shop: item.shop.name,
+						location: item.shop.location.name,
+						image: item.image
+							? getImageBase64(item.image)
+							: food.image
+								? getImageBase64(food.image)
+								: null,
+					},
+					upvotes: 0,
+					downvotes: 0,
+				};
+			});
+
+			return result;
+		} catch (e) {
+			console.log(e);
+			const message = e instanceof Error ? e.message : (e as string);
+			return { success: false, message };
+		}
+	},
+	getPosts: async (): Promise<Post[]> => {
+		const posts = await prisma.post.findMany({
+			where: {
+				archived: false,
+			},
+			include: {
+				creator: {
+					select: {
+						id: true,
+						name: true,
+						image: true,
+					},
+				},
+				item: {
+					select: {
+						priceMin: true,
+						priceMax: true,
+						image: true,
+						shop: {
+							select: {
+								name: true,
+								location: { select: { name: true } },
+							},
+						},
+						food: {
+							select: {
+								name: true,
+								image: true,
+								tags: { select: { name: true } },
+							},
+						},
+					},
+				},
+			},
+		});
+
+		const postsVote = await prisma.vote.groupBy({
+			by: ["postId", "upvote"],
+			where: {
+				post: { archived: false },
+			},
+			_count: true,
+		});
+
+		const voteMap = new Map<string, { upvotes: number; downvotes: number }>();
+		postsVote.forEach((p) => {
+			const existing = voteMap.get(p.postId) ?? { upvotes: 0, downvotes: 0 };
+
+			if (p.upvote) existing.upvotes = p._count;
+			else existing.downvotes = p._count;
+
+			voteMap.set(p.postId, existing);
+		});
+
+		const formattedPost = posts.map((p) => {
+			const votes = voteMap.get(p.id) ?? { upvotes: 0, downvotes: 0 };
+			const food = p.item.food;
+
+			return {
+				...p,
+				...votes,
+				food: {
+					name: food.name,
+					tags: food.tags.map((t) => t.name),
+					shop: p.item.shop.name,
+					location: p.item.shop.location.name,
+					image: p.item.image
+						? getImageBase64(p.item.image)
+						: food.image
+							? getImageBase64(food.image)
+							: null,
+				},
+			};
+		});
+		return formattedPost;
+	},
+	votePosts: async (userId: string, postId: string, isUpvote: boolean) => {
+		// If user didn't vote yet -> create vote
+		// If user already vote and vote the same -> delete vote
+		// If user vote different -> modify vote
+
+		try {
+			const user = await prisma.user.findUnique({ where: { id: userId } });
+			if (!user) throw new Error("User not found");
+
+			const existingVote = await prisma.vote.findUnique({
+				where: {
+					userId_postId: { userId, postId },
+				},
+			});
+
+			// If user didn't vote yet -> create vote
+			if (!existingVote) {
+				const vote = await prisma.vote.create({
+					data: { userId, postId, upvote: isUpvote },
+				});
+
+				return { success: true, vote };
+			}
+			// If user already vote and vote the same -> delete vote
+			if (existingVote.upvote === isUpvote) {
+				await prisma.vote.delete({
+					where: { id: existingVote.id },
+				});
+
+				return { success: true, vote: null };
+			}
+
+			// If user vote different -> modify vote
+			const vote = await prisma.vote.update({
+				where: { id: existingVote.id },
+				data: { upvote: isUpvote },
+			});
+
+			return { success: true, vote };
+		} catch (e) {
+			console.log(e);
+			const message = e instanceof Error ? e.message : (e as string);
+			return { success: false, message };
+		}
+	},
+};
